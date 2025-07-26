228a7228ef385d602beb5c38ece04eabd8d5b653
Daniel Ibarra <dannibar201@gmail.com>
2025-07-25 23:12:28 -0400

    Added action listeners to all buttons to appear on calculationField(JTextField) to mimic the sequence of numbers and operators for a calculators calculation. The resultTextArea(JTextArea) only shows the current number inputed and the result so everytime a number is clicked it appears in the text field. Next step is to find a way  to calculate two operands make it appear on the resultTextArea(JTextArea) and also showcase the correct sequence of numbers that calculated that result in the calculationField(JTextField)

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,6 +6,9 @@ public class Calculator extends JFrame{
 
     private JFrame frame;
     private JPanel keypadJPanel;
+    private JTextField calculationField;
+    private JTextArea resultTextArea;
+    
 
     private JButton num1;
     private JButton num2;
@@ -21,7 +24,7 @@ public class Calculator extends JFrame{
     private JButton percent;
 
     private JButton subtract;
-    private JButton add;
+    private JButton addition;
     private JButton multiply;
     private JButton divide;
     private JButton clear;
@@ -31,33 +34,43 @@ public class Calculator extends JFrame{
     
     public Calculator(){
         this.frame = new JFrame();
-        this.frame.setSize(400, 400);
+        this.frame.setSize(300, 400);
         this.frame.setLayout(new BoxLayout(this.frame.getContentPane(), BoxLayout.Y_AXIS));
         this.frame.setMinimumSize(new Dimension(300, 400));
 
         JLabel CalculaterLabel = new JLabel("Calculator");
-        frame.add(CalculaterLabel);
 
-        JTextArea resultJTextArea = new JTextArea("test", 1, 1);
-        resultJTextArea.setLineWrap(true);
-        frame.add(resultJTextArea);
-        resultJTextArea.setMinimumSize(new Dimension(frame.getContentPane().getWidth(), 150));
+        JTextField calculationField = new JTextField();
+        this.calculationField = calculationField;
+        calculationField.setMinimumSize(new Dimension(frame.getContentPane().getWidth(), 50));
+        calculationField.setEditable(false);
+        calculationField.setBackground(Color.LIGHT_GRAY);
+        calculationField.setFocusable(false);
+        calculationField.setHorizontalAlignment(JTextField.RIGHT);
+        
+        JTextArea resultTextArea = new JTextArea(1, 1);
+        this.resultTextArea = resultTextArea;
+        resultTextArea.setLineWrap(true);
+        resultTextArea.setMinimumSize(new Dimension(frame.getContentPane().getWidth(), 150));
 
         this.keypadJPanel = new JPanel();
         keypadJPanel.setLayout(new GridBagLayout());
-        keypadJPanel.setPreferredSize(new Dimension(frame.getContentPane().getWidth(), 500));
-
+        keypadJPanel.setPreferredSize(new Dimension(frame.getContentPane().getWidth(), 200));
         
-        addAllButtons();
+        addAllButtons(resultTextArea);
 
+        frame.add(CalculaterLabel);
+        frame.add(calculationField);
+        frame.add(resultTextArea);
         frame.add(keypadJPanel);
+        
         frame.setVisible(true);
 
         frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
     }
 
-    public void addAllButtons() {
+    public void addAllButtons(JTextArea result) {
 
         GridBagConstraints keyPadGridConstraints = new GridBagConstraints();
         keyPadGridConstraints.fill = GridBagConstraints.BOTH;
@@ -67,46 +80,115 @@ public class Calculator extends JFrame{
         this.num1 = new JButton("1");
         keyPadGridConstraints.gridx = 0;
         keyPadGridConstraints.gridy = 0;
+        num1.addActionListener(e -> {
+            result.setText("1");
+            calculationField.setText(calculationField.getText() + "1");
+        });
         keypadJPanel.add(num1, keyPadGridConstraints);
 
         this.num2 = new JButton("2");
         keyPadGridConstraints.gridx = 1;
+        num2.addActionListener(e -> {
+            result.setText("2");
+            calculationField.setText(calculationField.getText() + "2");
+        });
         keypadJPanel.add(num2, keyPadGridConstraints);
 
         this.num3 = new JButton("3");
         keyPadGridConstraints.gridx = 2;
+        num3.addActionListener(e -> {
+            result.setText("3");
+            calculationField.setText(calculationField.getText() + "3");
+        });
         keypadJPanel.add(num3, keyPadGridConstraints);
 
         this.num4 = new JButton("4");
         keyPadGridConstraints.gridx = 0;  // Column 0
         keyPadGridConstraints.gridy = 1;
+        num4.addActionListener(e -> {
+            result.setText("4");
+            calculationField.setText(calculationField.getText() + "4");
+        });
         keypadJPanel.add(num4, keyPadGridConstraints);
 
         this.num5 = new JButton("5");
         keyPadGridConstraints.gridx = 1;
+        num5.addActionListener(e -> {
+            result.setText("5");
+            calculationField.setText(calculationField.getText() + "5");
+        });
         keypadJPanel.add(num5, keyPadGridConstraints);
 
         this.num6 = new JButton("6");
         keyPadGridConstraints.gridx = 2;
+        num6.addActionListener(e -> {
+            result.setText("6");
+            calculationField.setText(calculationField.getText() + "6");
+        });
         keypadJPanel.add(num6, keyPadGridConstraints);
 
         this.num7 = new JButton("7");
         keyPadGridConstraints.gridx = 0;
         keyPadGridConstraints.gridy = 2;
+        num7.addActionListener(e -> {
+            result.setText("7");
+            calculationField.setText(calculationField.getText() + "7");
+        });
         keypadJPanel.add(num7, keyPadGridConstraints);
 
         this.num8 = new JButton("8");
         keyPadGridConstraints.gridx = 1;
+        num8.addActionListener(e -> {
+            result.setText("8");
+            calculationField.setText(calculationField.getText() + "8");
+        });
         keypadJPanel.add(num8, keyPadGridConstraints);
 
         this.num9 = new JButton("9");
         keyPadGridConstraints.gridx = 2;
+        num9.addActionListener(e -> {
+            result.setText("9");
+            calculationField.setText(calculationField.getText() + "9");
+        });
         keypadJPanel.add(num9, keyPadGridConstraints);
 
-        this.num9 = new JButton("0");
+        this.num0 = new JButton("0");
         keyPadGridConstraints.gridx = 1;
         keyPadGridConstraints.gridy = 3;
-        keypadJPanel.add(num9, keyPadGridConstraints);
+        num0.addActionListener(e -> {
+            result.setText("0");
+            calculationField.setText(calculationField.getText() + "0");
+        });
+        keypadJPanel.add(num0, keyPadGridConstraints);
+
+        this.divide = new JButton("/");
+        keyPadGridConstraints.gridx = 3;
+        keyPadGridConstraints.gridy = 0;
+        divide.addActionListener(e -> 
+        calculationField.setText(calculationField.getText() + " / "));
+        keypadJPanel.add(divide, keyPadGridConstraints);
+
+        this.multiply = new JButton("X");
+        keyPadGridConstraints.gridy = 1;
+        multiply.addActionListener(e -> calculationField.setText(calculationField.getText() + " X "));
+        keypadJPanel.add(multiply, keyPadGridConstraints);
+
+        this.subtract = new JButton("-");
+        keyPadGridConstraints.gridy = 2;
+        subtract.addActionListener(e -> calculationField.setText(calculationField.getText() + " - "));
+        keypadJPanel.add(subtract, keyPadGridConstraints);
+
+        this.addition = new JButton("+");
+        keyPadGridConstraints.gridy = 3;
+        addition.addActionListener(e -> calculationField.setText(calculationField.getText() + " + "));
+        keypadJPanel.add(addition, keyPadGridConstraints);
+
+        this.equals = new JButton("=");
+        keyPadGridConstraints.gridx = 0;
+        keyPadGridConstraints.gridy = 4;
+        keyPadGridConstraints.gridwidth = GridBagConstraints.RELATIVE;
+        equals.addActionListener(e -> calculationField.setText(calculationField.getText() + " = "));
+        keypadJPanel.add(equals, keyPadGridConstraints); 
 
     }
     public static void main(String[] args) {
